c6caf5cf798680158f8295336606aa3c0614fc63
Suren, Ecem <ecem.suren@adesso.com.tr>
2022-12-05 17:51:03 +0300

    discover screen images changed and logo added on who we are screen

--- app/src/main/java/com/deneyehayir/deneysiz/scene/whoweare/WhoWeAreScreen.kt
+++ app/src/main/java/com/deneyehayir/deneysiz/scene/whoweare/WhoWeAreScreen.kt
@@ -3,16 +3,7 @@ package com.deneyehayir.deneysiz.scene.whoweare
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -25,6 +16,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -163,17 +155,26 @@ private fun WhoWeAreScreenContent(
 fun WhoWeAreTopBar(
     onBack: () -> Unit
 ) {
-    Row(modifier = Modifier.fillMaxWidth()) {
-        Icon(
-            modifier = Modifier
-                .padding(16.dp)
-                .clickable { onBack() },
-            painter = painterResource(id = R.drawable.ic_back),
+    Icon(
+        modifier = Modifier
+            .padding(16.dp)
+            .clickable { onBack() },
+        painter = painterResource(id = R.drawable.ic_back),
+        contentDescription = null,
+        tint = DarkBlue
+    )
+
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+         verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_inexperienced_logo),
             contentDescription = null,
-            tint = DarkBlue
-        )
+            contentScale = ContentScale.Fit,
+            )
         Text(
-            modifier = Modifier.align(Alignment.CenterVertically),
             text = stringResource(id = R.string.top_bar_title_who_we_are),
             color = DarkTextColor,
             fontWeight = FontWeight.ExtraBold,
